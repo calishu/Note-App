667fc212d20ba522dec8dccff99b01916d3c0e69
calishu <danny.kretschmann@proton.me>
2025-03-28 21:15:53 +0100

    Upload src files

---
+++ src/main/java/dev/calishu/noteapp/db/Database.java
@@ -0,0 +1,29 @@
+package dev.calishu.noteapp.db;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity @Data
+@Table(name="noteapp")
+public class Database {
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    @Column private String text;
+    @Column private Integer date;
+    
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public Integer getDate() { return date; }
+    public void setDate(Integer date) { this.date = date; }
+
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+
+}
---
+++ src/main/java/dev/calishu/noteapp/db/DatabaseRepo.java
@@ -0,0 +1,7 @@
+package dev.calishu.noteapp.db;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DatabaseRepo extends CrudRepository<Database,Integer> {
+
+}
